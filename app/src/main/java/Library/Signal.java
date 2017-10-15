e2c00375668aa743c08bff80bcc4f4f5e2ab3381
rsengo42 <rsengo42@gmail.com>
2017-10-16 03:44:25 +0700

    Edit User

--- app/src/main/java/Library/Signal.java
+++ app/src/main/java/Library/Signal.java
@@ -1,9 +1,12 @@
 package Library;
+import java.time;
 
 /**
  * Created by ytgv8b on 16.10.2017.
  */
 
 public abstract class Signal implements IEdite {
-
+    protected String _name;
+    //protected LocalTime
+    //gfbgfhjfjfhjg
 }
