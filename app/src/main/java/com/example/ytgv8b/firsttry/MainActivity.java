fe996252656e8bb0fc99463f754ab9f0288cf019
rsengo42 <rsengo42@gmail.com>
2017-11-12 03:37:53 +0700

    Lib

--- app/src/main/java/com/example/ytgv8b/firsttry/MainActivity.java
+++ app/src/main/java/com/example/ytgv8b/firsttry/MainActivity.java
@@ -22,53 +22,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PreferenceHelper.init(this);
-
-
-        final Button save = (Button)findViewById(R.id.saveButton);
-        final Button load = (Button)findViewById(R.id.loadButton);
-        final TextView textView = (TextView)findViewById(R.id.Text);
-        final Button db = (Button)findViewById(R.id.dbButton);
-
-        save.setOnClickListener(v ->
-        {
-            User user = (User) User.getInstance();
-            user.setEmail("User@gmail.com");
-            user.setName("User");
-            user.setMoneyQuantity(1);
-            UserInterface userInterface = (UserInterface) user.getUserInterface();
-            userInterface.setFontSize(FontSize.NORMAL);
-            userInterface.setLanguage(Language.RUSSIAN);
-            PreferenceHelper.writePreference();
-
-            Realm.init(this);
-            RealmConfiguration config = new RealmConfiguration.Builder().name("myrealm.realm").build();
-            Realm.setDefaultConfiguration(config);
-            Realm realm = Realm.getDefaultInstance();
-
-            realm.beginTransaction();
-            Ring ring1 = new Ring();
-            ring1.setUserEmail("ghjghjgjgj");
-            Ring ring = realm.copyToRealm(ring1);
-            realm.commitTransaction();
-
-            realm.close();
-
-            textView.setText("Done");
-        });
-
-        load.setOnClickListener(v ->
-        {
-            User user = (User) User.getInstance();
-            textView.setText(user.getEmail());
-        });
-
-        db.setOnClickListener(v ->
-        {
-            /***DataBaseHelper helper = new DataBaseHelper(this);
-            User user = (User) User.getInstance();
-            textView.setText(user.getRings().get(0).getUserEmail());***/
-        });
-
     }
 }
