5f42cf18ead1163a20f9472a0e99e21dda288d6d
swash66 <swash5@rambler.ru>
2018-01-13 06:17:18 +0700

    Builders

--- app/src/main/java/Threads/MusicThread.java
+++ app/src/main/java/Threads/MusicThread.java
@@ -15,8 +15,10 @@ import Library.Signals.Ring;
  */
 
 public class MusicThread extends Thread {
-    IRing signal;
-    Context context;
+    private IRing signal;
+    private Context context;
+    private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener;
+    private SoundPool soundPool;
 
     public MusicThread(Context context, IRing signal) {
         super("Music thread");
@@ -31,7 +33,15 @@ public class MusicThread extends Thread {
         AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
 
         int melodyID = signal.getMelody();
-        float volume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+        float tryVolume;
+
+        try {
+            tryVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+        }  catch (NullPointerException ex) {
+            tryVolume = 1;
+        }
+
+        final float volume = tryVolume;
 
         AudioAttributes.Builder audioAttributesBuilder = new AudioAttributes.Builder();
         audioAttributesBuilder.setUsage(AudioAttributes.USAGE_ALARM)
@@ -43,21 +53,46 @@ public class MusicThread extends Thread {
         SoundPool.Builder soundPullBuilder = new SoundPool.Builder();
         soundPullBuilder.setMaxStreams(1)
                 .setAudioAttributes(audioAttributes);
-        SoundPool soundPool = soundPullBuilder.build();
+        soundPool = soundPullBuilder.build();
 
-        // TODO: 13.01.2018 загрузка мелодии
-        // TODO: 13.01.2018 audio focus
+        soundPool.load(context, melodyID, 1);
 
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                soundPool.play(0, volume, volume, 1, -1, 1);
+                int focus = audioManager.requestAudioFocus(audioFocusChangeListener,
+                        AudioManager.STREAM_RING, AudioManager.AUDIOFOCUS_GAIN);
+
+                if (focus == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    soundPool.play(melodyID, volume, volume, 1, -1, 1);
+                }
             }
         });
+
+        //аудио фокус
+        audioFocusChangeListener = focusChange -> {
+                        switch (focusChange) {
+                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                                soundPool.pause(0);
+                                break;
+                            case AudioManager.AUDIOFOCUS_GAIN:
+                                try {
+                                    soundPool.resume(0);
+                                } catch (Exception ex) {
+                                    soundPool.play(melodyID, volume, volume,
+                                            1, -1, 1);
+                                }
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS:
+                                soundPool.stop(0);
+                        }
+                    };
     }
 
     @Override
     public void interrupt() {
         super.interrupt();
+        soundPool.stop(0);
+        soundPool.release();
     }
 }
