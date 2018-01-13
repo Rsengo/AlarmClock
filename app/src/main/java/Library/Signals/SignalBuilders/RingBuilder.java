3b6b8ee1bad6283f0edc5de2ee2222681271f1ba
rsengo42 <rsengo42@gmail.com>
2018-01-14 01:53:15 +0700

    Priority

--- app/src/main/java/Library/Signals/SignalBuilders/RingBuilder.java
+++ app/src/main/java/Library/Signals/SignalBuilders/RingBuilder.java
@@ -92,7 +92,7 @@ public class RingBuilder extends SignalFactory {
 
         if (signal.getMelody() == null)
         {
-            signal.setMelody(preferenceHelper.loadMelodies().get(4));
+            signal.setMelody(preferenceHelper.loadMelodies().get(0));
         }
 
         return signal;
