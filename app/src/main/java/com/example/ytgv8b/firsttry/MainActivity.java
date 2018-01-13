bfae7dfcbbe49ab909a2fb21e585cb0c557142db
rsengo42 <rsengo42@gmail.com>
2018-01-14 02:28:10 +0700

    Priority

--- app/src/main/java/com/example/ytgv8b/firsttry/MainActivity.java
+++ app/src/main/java/com/example/ytgv8b/firsttry/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PreferenceHelper.init(this);
+        /*PreferenceHelper.init(this);
         DataBaseHelper.init(this);
 
         PreferenceHelper preferenceHelper = PreferenceHelper.getInstance();
@@ -81,6 +81,6 @@ public class MainActivity extends AppCompatActivity {
                     + "; "
                     + String.valueOf(notification.getCloseDate().getSeconds());
             textView.setText(s);*/
-        });
+        //});*/
     }
 }
