884b8f897b7ab4f782509e867382e014b91073b5
rsengo42 <rsengo42@gmail.com>
2018-01-14 03:24:30 +0700

    Priority

--- app/src/main/java/com/example/ytgv8b/firsttry/Services/NotificationReceiver.java
+++ app/src/main/java/com/example/ytgv8b/firsttry/Services/NotificationReceiver.java
@@ -22,7 +22,6 @@ public class NotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("Start", "start");
 
         DataBaseHelper.init(context);
 
@@ -30,31 +29,15 @@ public class NotificationReceiver extends BroadcastReceiver {
 
         int id = intent.getIntExtra("id", -1);
 
-        Log.e("Search", "start");
-
         INotification notification = dataBaseHelper.getNotification(id);
 
-        Log.e("Search", "stop");
-
-        // TODO: 12.01.2018 MainMenu
-        Intent notificationActivity = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, notification.getId(),
-                notificationActivity, 0);
-
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-
-        Log.e("Bitmap", "start");
 
         Resources res = context.getResources();
 
         Bitmap bitmap = BitmapFactory.decodeResource(res, R.drawable.ikonka_notif);
 
-        Log.e("Bitmap", "stop");
-
-        Log.e("Builder", "start");
-
-        builder.setContentIntent(pendingIntent)
-                .setSmallIcon(R.mipmap.ikonka)
+        builder.setSmallIcon(R.mipmap.ikonka)
                 .setContentTitle(notification.getName())
                 .setContentText(notification.getDescription())
                 .setLargeIcon(bitmap)
@@ -63,15 +46,9 @@ public class NotificationReceiver extends BroadcastReceiver {
                 .setAutoCancel(true)
                 .setPriority(notification.getPriority());
 
-        Log.e("Builder", "stop");
-
-        Log.e("Notif", "start");
-
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(notification.getId(), builder.build());
-
-        Log.e("Notif", "stop");
 
         notification.recountSignalTime(context);
         dataBaseHelper.saveRecursive(notification);
